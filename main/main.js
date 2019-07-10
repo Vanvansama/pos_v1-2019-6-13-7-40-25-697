56bb007f3cc48c8757ca1b332e2363129b999b3f
LIUST3 <stefan.liu@oocl.com>
2019-07-10 15:38:23 +0800

    init and test isBarCodeValid method

--- main/main.js
+++ main/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+function printReceipt(inputs) {
+  console.log(isBarCodeValid(inputs));
+  // console.log(createReceipt(inputs));
+}
+
+function isBarCodeValid(barcodes) {
+  const database = loadAllItems();
+  let flag = true;
+  const barcodeList = database.map((data) => data.barcode);
+  barcodes.map((barcode) => barcode.slice(1,10)).forEach(barcode => {
+    if (barcodeList.indexOf(barcode) === -1) {
+      flag = false;
+    }
+  });
+  return flag;
+}
